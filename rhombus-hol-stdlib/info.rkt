037f7c25e75bc983b0fa00b1b423cfc9387c9e9a
Masaya Taniguchi <ta2gch@gmail.com>
2026-09-09 20:56:22 +0900

    Add proved Rhombus HOL standard library

---
+++ rhombus-hol-stdlib/info.rkt
@@ -0,0 +1,11 @@
+#lang info
+
+(define collection 'multi)
+(define deps '("base"
+               ["rhombus-lib" #:version "1.1"]
+               "rhombus-hol-lib"
+               "rhombus-hol-kernel"))
+(define pkg-desc "proved standard library for Rhombus/HOL")
+(define license '0BSD)
+(define version "0.1")
+(define language-families '("Rhombus"))
